e15aed6a02039028e9cbb68afcc4c259645d1631
gitlabNamBoongEh <67003390+NamBoongEh@users.noreply.github.com>
2022-02-18 11:50:30 +0900

    #fix
    - coin character 비충돌 문제 수정중
    - z축 확인 필요

--- Assets/Script/Coin.cs
+++ Assets/Script/Coin.cs
@@ -19,4 +19,20 @@ public class Coin : MonoBehaviour
         if (collision.gameObject.tag == "Player")
             Destroy(gameObject);
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("2D trigger ø©±‚ ¥Í¿Ω");
+
+        if (collision.gameObject.tag == "Player")
+            Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("trigger ø©±‚ ¥Í¿Ω");
+
+        if (other.gameObject.tag == "Player")
+            Destroy(gameObject);
+    }
 }
